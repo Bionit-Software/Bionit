828f2dc87a41b7db437060819289baee3a0bab73
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-11-14 14:53:59 -0300

    Operaciones ABM basicas entidad zonas mas

--- src/components/Dialog/AddZoneDialog.jsx
+++ src/components/Dialog/AddZoneDialog.jsx
@@ -18,7 +18,30 @@ export const AddZoneDialog = NiceModal.create((props) => {
         modal.hide();
       }}
     >
-      <div className="bg-gray-100 shadow-lg p-32 border-1 border-black"></div>
+      <div className="bg-gray-100 shadow-lg p-32 border-1 border-black">
+        <input
+          value={zoneName}
+          onChange={(e) => {
+            setZoneName(e.target.value);
+          }}
+          placeholder="Nombre de la zona"
+        />
+        <input
+          value={zoneDescription}
+          onChange={(e) => {
+            setZoneDescription(e.target.value);
+          }}
+          placeholder="Descripción de la zona"
+        />
+        <button
+          onClick={() => {
+            addZone(zoneName, zoneDescription);
+            modal.hide();
+          }}
+        >
+          Agregar
+        </button>
+      </div>
     </BaseDialog>
   );
 });
--- src/hooks/useZones.jsx
+++ src/hooks/useZones.jsx
@@ -1,14 +1,44 @@
-import { addDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
 import React from "react";
 import { db } from "../db/database";
 
 export const useZones = () => {
+  const [zones, setZones] = React.useState([]);
   const addZone = async (zoneName, zoneDescription) => {
-    await addDoc(db, "zones", {
+    await addDoc(collection(db, "zones"), {
+      name: zoneName,
+      description: zoneDescription,
+      patients: [],
+      nurses: [],
+    });
+  };
+  const deleteZone = async (zoneId) => {
+    await deleteDoc(doc(db, "zones", zoneId));
+  };
+
+  const editZone = async (zoneId, zoneName, zoneDescription) => {
+    await updateDoc(doc(db, "zones", zoneId), {
       name: zoneName,
       description: zoneDescription,
     });
   };
 
-  return { addZone };
+  React.useEffect(() => {
+    onSnapshot(collection(db, "zones"), (querySnapshot) => {
+      const zones = [];
+      querySnapshot.forEach((doc) => {
+        zones.push({ id: doc.id, ...doc.data() });
+      });
+      setZones(zones);
+    });
+  }, []);
+
+  return { zones, addZone, deleteZone, editZone };
 };
