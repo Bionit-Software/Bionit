384d1326906a38233c550bdc49097b50d14e8bae
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-11-16 00:05:09 -0300

    css fondo react tostify notificaciones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,64 +1,80 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import { useAuth } from "../context/AuthContext";
+import {
+  addNotificationDoc,
+  useNotificationManager,
+} from "../hooks/useNotifications";
 import { useUser } from "../hooks/useUsers";
-import { motion } from "framer-motion";
 export default function Navbar() {
-    const { logout, user } = useAuth();
-    const handleLogout = () => {
-        logout();
-    };
-    const navigate = useNavigate();
-    const { userData } = useUser(user.uid);
-    return (
-        <div className="container h-full w-2/12 p-2 shadow-lg">
-            <motion.div
-                initial={{ x: -100, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{
-                    duration: 0.7,
-                    delay: 0.6,
-                    ease: [0, 0.71, 0.2, 1.01],
-                }}
-                className="container flex flex-col gap-6">
-                <button
-                    className="bg-gray-100 shadow-lg p-6 w-full"
-                    onClick={() => {
-                        navigate("/patients");
-                    }}
-                >
-                    PACIENTES
-                </button>
-                {userData?.rol === "admin" ? (
-                    <div className="container flex flex-col gap-6">
-                        <button
-                            className="bg-gray-100 shadow-lg p-6 w-full"
-                            onClick={() => {
-                                navigate("/zones");
-                            }}
-                        >
-                            Zonas
-                        </button>
-                        <button
-                            className="bg-gray-100 shadow-lg p-6 w-full"
-                            onClick={() => navigate("/dashboard")}
-                        >
-                            Dashboard
-                        </button>
-                    </div>
-                ) : null}
-                <button
-                    className="bg-gray-100 shadow-lg p-6 w-full"
+  const { logout, user } = useAuth();
 
-                >
-                    Simular Llamada
-                </button>
-                <button onClick={handleLogout} 
-                    className="bg-red-500 shadow-lg shadow-red-500/50 text-white w-full 
-                    font-semibold rounded-lg p-6 ">
-                    Cerrar Sesión
-                </button>
-            </motion.div>
-        </div>
-    )
+  const { handleCall } = useNotificationManager({
+    user: user,
+  });
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  const navigate = useNavigate();
+  const { userData } = useUser(user.uid);
+
+  return (
+    <div className="container h-full w-2/12 p-2 shadow-lg">
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          duration: 0.7,
+          delay: 0.6,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+        className="container flex flex-col gap-6"
+      >
+        <button
+          className="bg-gray-100 shadow-lg p-6 w-full"
+          onClick={() => {
+            navigate("/patients");
+          }}
+        >
+          PACIENTES
+        </button>
+        {userData?.rol === "admin" ? (
+          <div className="container flex flex-col gap-6">
+            <button
+              className="bg-gray-100 shadow-lg p-6 w-full"
+              onClick={() => {
+                navigate("/zones");
+              }}
+            >
+              Zonas
+            </button>
+            <button
+              className="bg-gray-100 shadow-lg p-6 w-full"
+              onClick={() => navigate("/dashboard")}
+            >
+              Dashboard
+            </button>
+          </div>
+        ) : null}
+        <button
+          className="bg-gray-100 shadow-lg p-6 w-full"
+          onClick={handleCall}
+        >
+          Simular Llamada
+        </button>
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 shadow-lg shadow-red-500/50 text-white w-full 
+                    font-semibold rounded-lg p-6 "
+        >
+          Cerrar Sesión
+        </button>
+      </motion.div>
+      <ToastContainer />
+    </div>
+  );
 }
--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import { arrayUnion, updateDoc } from "firebase/firestore";
+import React from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "../components/Navbar";
 
 export default function Dashboard() {
   return (
     <div className="container h-full w-full flex">
-      <Navbar/>
-      <div className="container h-full w-10/12">
-        Dashboard
-      </div>
+      <Navbar />
+      <div className="container h-full w-10/12">Dashboard</div>
     </div>
-  )
+  );
 }
